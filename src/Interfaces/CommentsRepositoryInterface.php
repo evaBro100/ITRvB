1aa4d5fdefa5f5c8b8c18edf1af0df8f64530980
evaBro100 <kharchenko.eva@gmail.com>
2025-01-09 21:42:07 +0500

    feat.: вынесла в отд папку интерфейс

--- src/Interfaces/CommentsRepositoryInterface.php
+++ src/Interfaces/CommentsRepositoryInterface.php
@@ -1,10 +1,4 @@
-<?php
-
-interface PostsRepositoryInterface {
-    public function get(string $uuid): ?Post;
-    public function save(Post $post): void;
-}
-
+<?php 
 interface CommentsRepositoryInterface {
     public function get(string $uuid): ?Comment;
     public function save(Comment $comment): void;
---
+++ src/Interfaces/PostsRepositoryInterface.php
@@ -0,0 +1,5 @@
+<?php
+interface PostsRepositoryInterface {
+    public function get(string $uuid): ?Post;
+    public function save(Post $post): void;
+}
